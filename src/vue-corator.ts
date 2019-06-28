a30f9dd4c6ba54ac27205dcebd827676195dcd40
joon <jdj610@gmail.com>
2019-06-28 21:03:12 +0900

    version 1.0.0

---
+++ src/vue-corator.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import { createDecorator } from 'vue-class-component';
+export function Style(refKey: string) {
+    return createDecorator((options, key) => {
+        options.computed = options.computed || {};
+        options.computed[key] = {
+          cache: false,
+          get(this: Vue) {
+              // @ts-ignore
+            return this.$refs[refKey || key].style;
+          },
+        };
+      });
+  }
+
