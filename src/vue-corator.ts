72810a48030495d0e5c09b9c28bad9214cf397c4
jdj <jdj610@gmail.com>
2019-07-31 11:32:30 +0900

    bug fix

--- src/vue-corator.ts
+++ src/vue-corator.ts
@@ -2,68 +2,68 @@ import Vue from 'vue'
 import { createDecorator } from 'vue-class-component';
 
 export function Style() {
-  return (target: Vue, key: string, descriptor: PropertyDescriptor) => {
-    const style = descriptor.value();
-    const newComponent = {
-      // @ts-ignore
-      render(createElement: any) {
-        return createElement('style',
-        {attrs: { type: 'text/css', lang: 'css'}},
-        // @ts-ignore
-        [this.$slots.default || style]);
-      }
+    return (target: Vue, key: string, descriptor: PropertyDescriptor) => {
+        const style = descriptor.value();
+        const newComponent = {
+            // @ts-ignore
+            render(createElement: any) {
+                return createElement('style',
+                    { attrs: { type: 'text/css', lang: 'css' } },
+                    // @ts-ignore
+                    [this.$slots.default || style]);
+            }
+        };
+        createDecorator((options, k) => {
+            options.components = options.components || {};
+            options.components[k] = newComponent;
+        })(target, key);
     };
-    createDecorator((options, k) => {
-        options.components = options.components || {};
-        options.components.vStyle = newComponent;
-    })(target, key);
-  };
 }
 
 export function NextTick() {
-  return (target: Vue, key: string, descriptor: any) => {
-    const method = descriptor.value;
-    descriptor.value = () => {
-        Vue.nextTick(() => {
-            method();
-        });
-      };
-    const returnValue: any =  descriptor.value;
-    return descriptor.value();
-  };
+    return (target: Vue, key: string, descriptor: any) => {
+        const method = descriptor.value;
+        descriptor.value = () => {
+            Vue.nextTick(() => {
+                method();
+            });
+        };
+        const returnValue: any = descriptor.value;
+        return descriptor.value();
+    };
 }
 
 export function Render(componentName?: string) {
-  return (target: Vue, key: string, descriptor: PropertyDescriptor) => {
-    const method = descriptor.value;
-    const compiledTemplate = Vue.compile(method());
-    const execParameta = /(?=\(([^)]*)\))(?=[^,]+)/g;
-    const paramArray = execParameta.exec(method);
-    const parameta = paramArray !== null ? paramArray[1].replace(/\s*/g, '') : ''; // remove space
-    const props = parameta.split(',');
-    const newComponent = {
-      props,
-      render(createElement: any) {
-        return compiledTemplate.render.call(this, createElement);
-      }
+    return (target: Vue, key: string, descriptor: PropertyDescriptor) => {
+        const method = descriptor.value;
+        const compiledTemplate = Vue.compile(method());
+        const execParameta = /(?=\(([^)]*)\))(?=[^,]+)/g;
+        const paramArray = execParameta.exec(method);
+        const parameta = paramArray !== null ? paramArray[1].replace(/\s*/g, '') : ''; // remove space
+        const props = parameta.split(',');
+        const newComponent = {
+            props,
+            render(createElement: any) {
+                return compiledTemplate.render.call(this, createElement);
+            }
+        };
+        createDecorator((options, k) => {
+            options.components = options.components || {};
+            options.components[componentName || key] = newComponent;
+        })(target, key);
     };
-    createDecorator((options, k) => {
-      options.components = options.components || {};
-      options.components[componentName || key] = newComponent;
-    })(target, key);
-  };
 }
 
 export function ScopedId(key?: string) {
-  return createDecorator((options, k) => {
-      options.computed = options.computed || {};
-      options.computed[k] = {
-        cache: false,
-        get(this: Vue) {
-            // @ts-ignore
-          return  this.$options._scopeId;
-        },
-      };
+    return createDecorator((options, k) => {
+        options.computed = options.computed || {};
+        options.computed[k] = {
+            cache: false,
+            get(this: Vue) {
+                // @ts-ignore
+                return this.$options._scopeId;
+            },
+        };
     });
 }
 
